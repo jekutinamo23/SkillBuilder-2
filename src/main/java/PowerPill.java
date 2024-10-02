a9e338a84ed2daa8b89c810102cf4f2d801e4ef4
jekutinamo23 <jekutinamo23@students.desu.edu>
2024-10-01 21:45:35 -0400

    Skill

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -1,38 +1,52 @@
-
 /**
  * This class defines how power pills behave in
- * a game.  The purpose of a power pill is to simply
+ * a game. The purpose of a power pill is to simply
  * provide a character with power.
- *
- * @author (You)
- * @version (0.1)
  */
-public class PowerPill
-{
-    // TODO - replace this line with instruction from step 1
+public class PowerPill {
+    // Default power value for pills
+    public static final int DEFAULT_POWER = 10;
 
-    // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    // Instance variables
+    private int power;
+    private String name;
 
-    // constructors
+    // Constructor for a pill with a default power
+    public PowerPill(String name) {
+        this.name = name;
+        this.power = DEFAULT_POWER;
+    }
 
-    // TODO - replace this line with instruction from step 4
+    // Constructor for a pill with a specified power
+    public PowerPill(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
 
-    // TODO - replace this line with instruction from step 5
+    // Get the power of the pill
+    public int getPower() {
+        return power;
+    }
 
+    // Get the name of the pill
+    public String getName() {
+        return name;
+    }
 
-    // accessor methods
+    // Set a new power level for the pill
+    public void setPower(int power) {
+        this.power = power;
+    }
 
-    // TODO - replace this line with instruction from step 6
+    // Set a new name for the pill
+    public void setName(String name) {
+        this.name = name;
+    }
 
-
-    // mutator methods
-
-    // TODO - replace this line with instruction from step 7
-
-    // toString method
-
-    // TODO - replace this line with instruction from step 8
-
+    // Return a string representation of the pill
+    @Override
+    public String toString() {
+        return "PowerPill " + name + " = " + power;
+    }
 }
+
